64ebc3d94d5aeb26e80c5b5597c974b369fbf954
caiupi <caiupi@yahoo.it>
2022-04-10 03:26:07 +0200

    filtering versioning authentication

---
+++ restful-web.service/src/main/java/com/in28minutes/rest/webservices/restfulwebservice/versioning/Name.java
@@ -0,0 +1,30 @@
+package com.in28minutes.rest.webservices.restfulwebservice.versioning;
+
+public class Name {
+	private String firstName;
+	private String secondName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSecondName() {
+		return secondName;
+	}
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	public Name(String firstName, String secondName) {
+		super();
+		this.firstName = firstName;
+		this.secondName = secondName;
+	}
+	
+	public Name() {
+		
+	}
+	
+	
+
+}
---
+++ restful-web.service/src/main/java/com/in28minutes/rest/webservices/restfulwebservice/versioning/PersonV2.java
@@ -0,0 +1,25 @@
+package com.in28minutes.rest.webservices.restfulwebservice.versioning;
+
+public class PersonV2 {
+
+	private Name name;
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+	public PersonV2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+	
+}
